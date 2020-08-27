6d31131f23bc26cc1a6b269686e29672e9f13e27
ethan.zammit.19 <ethan.zammit.19@um.edu.mt>
2020-08-27 22:03:19 +0200

    added wee comment

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ downloadsPath = ""
 docs = ['pdf','docx','doc','pptx','ppt',] #split into presentations/pdfs and txt files
 data = ['csv','txt','json','yaml','xlsx','sql','html']
 media = ['png','jpg','mp3','mp4','gif']
-archives = ['zip','tar.xz','rar','7z','iso']
+archives = ['zip','tar.xz','rar','7z','iso'] #check for xz then check for a 'tar' before it
 runnables = ['exe','msi','jar','py','js'] 
 torrents = ['torrent']
 Other = []
+ignore = []
 
 def move(oldPath,newPath):
     print("moved")
