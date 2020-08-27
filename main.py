8352eb8336f1189623606b51a495fcc6dbf0d21d
ethan.zammit.19 <ethan.zammit.19@um.edu.mt>
2020-08-27 21:50:29 +0200

    starting filename classification

---
+++ main.py
@@ -0,0 +1,17 @@
+#should be quite a simple program thus one class should suffice
+import os,sys
+downloadsPath = ""
+
+docs = ['pdf','docx','doc','pptx','ppt',] #split into presentations/pdfs and txt files
+data = ['csv','txt','json','yaml','xlsx','sql','html']
+media = ['png','jpg','mp3','mp4','gif']
+archives = ['zip','tar.xz','rar','7z','iso']
+runnables = ['exe','msi','jar','py','js'] 
+torrents = ['torrent']
+Other = []
+
+def move(oldPath,newPath):
+    print("moved")
+
+def checkFolder():
+    downloadsPath
