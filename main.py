b13c371bcd402064dacdf096f7f134c206e9978e
Ethan Zammit <ethan.zammit.19@um.edu.mt>
2020-08-27 22:56:51 +0200

    added some more files

--- main.py
+++ main.py
@@ -2,11 +2,11 @@
 import os,sys
 downloadsPath = ""
 
-docs = ['pdf','docx','doc','pptx','ppt',] #split into presentations/pdfs and txt files
-data = ['csv','txt','json','yaml','xlsx','sql','html']
-media = ['png','jpg','mp3','mp4','gif']
+docs = ['pdf','docx','doc','pptx','ppt','pps','odp','rtf'] #split into presentations/pdfs and txt files
+data = ['csv','txt','json','yaml','xlsx','xls','xlsm','sql','html','data','xml','cfg']
+media = ['png','jpg','mp3','mp4','m4v','mkv','swf','flv','avi','gif','wav','bmp','jpeg','ico','ps','psd','svg']
 archives = ['zip','tar.xz','rar','7z','iso'] #check for xz then check for a 'tar' before it
-runnables = ['exe','msi','jar','py','js'] 
+runnables = ['exe','msi','jar','py','js','bat','c','cpp','h'] 
 torrents = ['torrent']
 Other = []
 ignore = []
